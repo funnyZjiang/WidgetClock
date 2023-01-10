370a44abbb8af40e1c63ae914a4f832feb2150b7
funnyZjiang <wo1645228@163.com>
2023-01-11 01:14:09 +0800

    Add files via upload

---
+++ ClockWidget/Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by zhengfeng1 on 2023/1/11.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ClockWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ClockWidget()
+    }
+}
